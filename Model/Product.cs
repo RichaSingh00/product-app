c40594054eeb61782790942e71532cc491108dc5
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:38:46 +0530

    Add project files.

---
+++ Model/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Model
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private double _discountPercent;
+
+        //setter methods
+        public void SetId(int id)
+        { _id = id; }
+        public void SetName(string name) 
+        { _name = name; }
+        public void SetPrice(double price) 
+        { _price = price; }
+        public void SetDiscountPercent(double discountPercent)
+        { _discountPercent = discountPercent; }
+
+        //getter methods
+        public int GetId() 
+        { return _id; }
+        public string GetName()
+        { return _name; }
+        public double GetPrice()
+        { return _price; }
+        public double GetDiscountPercent()
+        { return _discountPercent; }
+        public double DiscountCost()
+        {
+            double discountPrice = _price * (_discountPercent / 100);
+            return _price - discountPrice;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ProductApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.SetId(1);
+            product1.SetName("Product A");
+            product1.SetPrice(1000);
+            product1.SetDiscountPercent(10);
+           
+            Product product2 = new Product();
+            product2.SetId(2);
+            product2.SetName("Product B");
+            product2.SetPrice(2000);
+            product2.SetDiscountPercent(15);
+           
+            DisplayProduct(product1);
+            Console.WriteLine();
+
+            DisplayProduct(product2);
+            Console.WriteLine();
+        }
+        static void DisplayProduct(Product product)
+        { 
+            Console.WriteLine("The id of the product is: " + product.GetId());
+            Console.WriteLine("The name of the product is: " + product.GetName());
+            Console.WriteLine("The price of the product is: " + product.GetPrice());
+            Console.WriteLine("The discount percent offered is: " + product.GetDiscountPercent());
+            Console.WriteLine("The price after discount is: " + product.DiscountCost());
+        }
+ 
+    }
+}
